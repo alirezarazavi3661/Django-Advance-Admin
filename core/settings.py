31b333c2a2e286d49f4e66e53a86ba529310bcce
AlirezaRazavi <Alirezarazavi3661@gmail.com>
2023-10-05 15:53:18 +0330

    add static and media
    i have defined static and media roots and urls

--- core/settings.py
+++ core/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    # 'blog',
+     'blog',
 ]
 
 MIDDLEWARE = [
@@ -110,8 +110,18 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static'
+
+
+# Media files (CSS, JavaScript, Images)
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+# Staticfiles Directory
+STATICFILES_DIR = [
+    BASE_DIR / "staticfiles"
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

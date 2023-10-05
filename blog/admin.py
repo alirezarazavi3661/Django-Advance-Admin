e6e7f2e06a65211faa43092cf33ceba0ae6a04fb
AlirezaRazavi <Alirezarazavi3661@gmail.com>
2023-10-05 15:56:44 +0330

    customize blog/admin
    i have customized admin panel for blog app

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Category,Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    """
+    Customize admin panel for Posts
+    """
+    list_display = ['author','title','status','category','created_date','published_date']
+
+
+"""
+registration for Category model in admin panel
+"""
+admin.site.register(Category)
+
+"""
+registration for Post model in admin panel
+"""
+admin.site.register(Post)

67e373d2094a4e27e4ecc490d5fc4b6d381c900a
AlirezaRazavi <Alirezarazavi3661@gmail.com>
2023-10-05 14:32:43 +0330

    profile registration to admin panel
    i have registered the Profile class into admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,10 +1,24 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from accounts.models import User
+from accounts.models import User,Profile
+
+
+
+"""
+this is a Custom form that is not needed for now
+"""
+# class CustomUserCreationForm(UserCreationForm):
+#     class Meta:
+#         model = User
+#         fields = ("email",)
 
 
 class CustomUserAdmin(UserAdmin):
+    """
+    this is a class which customize the admin panel for more information about the user
+    """
     model = User
+    #add_form = CustomUserCreationForm
     list_display = ('email','first_name','is_superuser', 'is_active')
     list_filter = ('email', 'is_superuser', 'is_active')
     search_fields = ('email',)  # Corrected 'searching_fields' to 'search_fields'
@@ -16,6 +30,12 @@ class CustomUserAdmin(UserAdmin):
         ("Permissions", {
             "fields": ("is_staff", "is_active", "is_superuser","first_name"),
         }),
+        ("Group_Permissions", {
+            "fields": ("groups","user_permissions"),
+        }),
+        ("important_date", {
+            "fields": ("last_login",),
+        }),
     )
     add_fieldsets = (
         (None, {
@@ -25,8 +45,14 @@ class CustomUserAdmin(UserAdmin):
     )
 
 
+"""
+registration for adding the above class to panel
+"""
 admin.site.register(User,CustomUserAdmin)
-
+"""
+registration for adding the Profile section to admin 
+"""
+admin.site.register(Profile)
 
 
 

3e4f255b7e07133678b059cc1a95443d6836668b
AlirezaRazavi <Alirezarazavi3661@gmail.com>
2023-10-18 12:12:05 +0330

    create router
    instead of using urls, i have changed the urls.py using router because of changing view and using modelviweset

--- blog/api/v1/urls.py
+++ blog/api/v1/urls.py
@@ -1,10 +1,28 @@
 from . import views
 from django.urls import path,include
+from rest_framework.routers import DefaultRouter
 
 app_name = "api-v1"
-urlpatterns = [
 
-    path('post/',views.PostList,name="post-list"),
-    path('post/<int:id>/',views.PostDetail,name="post-detail")
+router = DefaultRouter()
+router.register('post',views.PostModelViewSet,basename='post')
+router.register('category',views.CategoryModelViewSet,basename = 'category')
+urlpatterns = router.urls
 
-    ]
+
+
+
+
+# urlpatterns = [
+#     path("post/",views.PostViewSet.as_view({'get':'list','post':'create'}),name = 'post-list'),
+#     path("post/<int:pk>/", views.PostViewSet.as_view({'get': 'retrieve','update':'put','patch':'partial_update','delete':'destroy'}), name='post-detail')
+#     # path('post/',views.PostList,name="post-list"),
+#     # path('post/<int:id>/',views.PostDetail,name="post-detail"),
+#     # path('post/', views.PostList.as_view(), name="post-list"),
+#     # path('post/<int:id>/',views.PostDetail.as_view(), name="post-detail"),
+#     ]
+
+
+
+
+

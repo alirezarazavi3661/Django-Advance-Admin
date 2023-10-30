1e406ffdd5f2d937a065b237a1888f7a8e3a0998
AlirezaRazavi <Alirezarazavi3661@gmail.com>
2023-10-30 21:27:11 +0330

    resend email urls
    i have added a urls for resend the verification email

--- accounts/api/v1/urls/accounts.py
+++ accounts/api/v1/urls/accounts.py
@@ -9,7 +9,7 @@ urlpatterns = [
     #activation
     path('activation/confirm/<str:token>',views.ActivationApiView.as_view(),name = 'activation'),
     #resend activation
-
+    path('activation/resend/',views.ActivationResendApiView.as_view(),name = 'activation-resend'),
     #registration
     path('registration/',views.RegistrationApiView.as_view(),name='registration'),
     #change-password
